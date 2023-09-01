6b177f2f290c68788e24f6412b7a14054f4418d1
Darren <darreno@Darrens-MBP.cg.shawcable.net>
2023-09-01 11:33:23 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <DynamicCssClasses />
+</template>
+
+<script>
+import DynamicCssClasses from './components/chapter9/Dynamic-css-classes.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DynamicCssClasses
+  }
+}
+</script>
